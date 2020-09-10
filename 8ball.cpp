7b603f85a4920bd6eb8771757fa61fdfddf61bae
Xavier Rivera <43128275+Archrayx@users.noreply.github.com>
2020-09-10 11:28:42 -0400

    Update 8ball.cpp

--- 8ball.cpp
+++ 8ball.cpp
@@ -4,27 +4,29 @@ using namespace std;
 
 int main()
 {
-  while(true){
-
-    int ran = (std::rand() % 3) + 1;
-    string answer = "";
-    if (ran == 1)
-      {
-        cout << "what is your question: " << endl;
-        cin >> answer;
-        cout << "no" << endl;
-      }
-    else if (ran == 2)
-      {
-        cout << "what is your question: " << endl;
-        cin >> answer;
-        cout << "yes" << endl;
-      }
-    else (ran == 3)
-      {
-        cout << "what is your question: " << endl;
-        cin >> answer;
-        cout << "maybe" << endl;
-      }
+    int ran = 0;
+    while(ran == 0)
+    {
+        ran = (std::rand() % 3) + 1;
+        string answer = "";
+        if (ran == 1)
+        {
+            cout << "what is your question: " << endl;
+            cin >> answer;
+            cout << "no" << endl;
+        }
+        else if (ran == 2)
+        {
+            cout << "what is your question: " << endl;
+            cin >> answer;
+            cout << "yes" << endl;
+        }
+        else if (ran == 3)
+        {
+            cout << "what is your question: " << endl;
+            cin >> answer;
+            cout << "maybe" << endl;
+        }
     }
+    return 0;
 }
